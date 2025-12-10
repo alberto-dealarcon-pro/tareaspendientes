211111ea3cf0a7228b4f2dcaa8a9ab2ccf9a84e8
alberto-dealarcon-pro <alberto.dealarcon.pro@iesmarquesdecomares.org>
2025-12-10 10:20:52 +0100

    asdf

---
+++ tareaspendientes/lang/en/block_tareaspendientes.php
@@ -0,0 +1,12 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU GPL v3 or later.
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Pending Tasks';
+$string['tareaspendientes:addinstance'] = 'Add a new Pending Tasks block';
+$string['tareaspendientes:myaddinstance'] = 'Add a new Pending Tasks block to My home';
+$string['blocktitle'] = 'Pending Tasks';
