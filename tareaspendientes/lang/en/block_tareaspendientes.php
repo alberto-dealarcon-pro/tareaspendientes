c891bab866fdd458a8e9d163ddc661f08fab1ab2
Alberto de Alarcón Sánchez. <71717744+alberto-dealarcon-pro@users.noreply.github.com>
2025-12-16 09:37:18 +0100

    sdf

--- tareaspendientes/lang/en/block_tareaspendientes.php
+++ tareaspendientes/lang/en/block_tareaspendientes.php
@@ -10,3 +10,4 @@ $string['pluginname'] = 'Pending Tasks';
 $string['tareaspendientes:addinstance'] = 'Add a new Pending Tasks block';
 $string['tareaspendientes:myaddinstance'] = 'Add a new Pending Tasks block to My home';
 $string['blocktitle'] = 'Pending Tasks';
+$string['notasks'] = 'There are no pending tasks to grade in this course.';
